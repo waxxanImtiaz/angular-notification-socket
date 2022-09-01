6dd8db8bd0f9daa721f6679fb5512512d49ba5fb
Imtiaz Ali <waxxan.imtiaz.123@gmail.com>
2022-09-01 16:06:55 +0500

    Order status update is implemented

--- src/app/shared/constants/base-url.constants.ts
+++ src/app/shared/constants/base-url.constants.ts
@@ -6,4 +6,6 @@ import { environment } from 'src/environments/environment';
 
 
 export const  WEBSOCKET_ENDPOINT = "http://localhost:5000/ws-notification";
-export const  WEBSOCKET_ORDERED_ELEMENTS = '/topic/elements';
+export const  WEBSOCKET_ELEMENTS_STATUS = '/topic/elements/status';
+export const  WEBSOCKET_NOTIFY_TOPIC = '/topic/elements';
+export const  WEBSOCKET_CALL_WAITER = '/topic/waiter';
